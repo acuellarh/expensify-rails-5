731f836a878e670ab42bad6eae7746003ff296b6
Alejandro <acuellarh@gmail.com>
2019-08-14 13:28:28 -0500

    Gravatar addded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :avatar
+
+  protected
+
+    def avatar
+      @avatar = user_signed_in? ? current_user.avatar_url : nil
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :expenses       
+  has_many :expenses 
+  
+  def avatar_url 
+    hash = Digest::MD5.hexdigest(email)
+    "http://www.gravatar.com/avatar/#{hash}?s=30&d=identicon"
+  end 
+  
 end
