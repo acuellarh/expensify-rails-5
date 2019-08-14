210f0bcadda3fbece6a58ae636fd22af373d751e
Alejandro <acuellarh@gmail.com>
2019-08-13 19:30:47 -0500

    Delete and Edit Expenses options

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,6 @@
 class ExpensesController < ApplicationController
+  before_action :current_expense, only: [:show, :edit, :update, :destroy]
+
   def index
     @tab = :expenses
     @expenses = Expense.all
@@ -19,12 +21,34 @@ class ExpensesController < ApplicationController
     end      
   end
 
+  def edit    
+  end 
+  
+  def update    
+    if  @expense.update(params_expenses)
+      flash[:notice] = "Gasto actualizado satisfactoriamente"
+      redirect_to expenses_path(@expense)
+    else
+      render :edit
+    end    
+  end
+
+
+  def destroy
+    if @expense.destroy
+      flash[:notice] = "Gasto eliminado satisfactoriamente"
+      redirect_to expenses_path
+    end      
+  end
+
   private
 
   def params_expenses
     params.require(:expense).permit(:id, :type_id, :date, :concept, :category_id, :amount).merge(user: current_user)
   end
 
-
+  def current_expense
+    @expense = Expense.find(params[:id])
+  end  
 
 end
