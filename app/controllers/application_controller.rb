7ccca8fee659c8f64ce7e12a0cc3c6c02083418a
Alejandro <acuellarh@gmail.com>
2019-08-24 15:07:30 -0500

    Just signed users can access to the platform

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :avatar
+  before_action :authenticate_user!
 
   protected
 
     def avatar
       @avatar = user_signed_in? ? current_user.avatar_url : nil
     end
+
+  
 
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -17,4 +17,6 @@ class Expense < ApplicationRecord
   belongs_to :type
   belongs_to :category
   belongs_to :user
+
+  
 end
