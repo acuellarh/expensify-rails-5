59786666d861ad68fb13095e2fbe6790f15f4d33
Alejandro <acuellarh@gmail.com>
2019-10-19 17:56:33 -0500

    Faker feature sintaxix adjusted

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,12 +20,9 @@ Expense.delete_all
 20.times do
   Expense.create(
     user_id: [1,34].sample,
-    #type_id: Faker::Number.between(from: 1, to: 4),
-    type_id: [1,2,3,4].sample,
-    #category_id: Faker::Number.between(from: 1, to: 4),
-    category_id: [1,2,3,4].sample,
-    #amount: Faker::Commerce.price(range: 0..100000.0),    
-    amount: [100, 200, 250, 400, 100].sample,        
+    type_id: Faker::Number.between(1,4),  
+    category_id: Faker::Number.between(1,4),           
+    amount: Faker::Commerce.price(0..100000.0),           
     concept: Faker::Lorem.sentence,
     date: Faker::Date.between(6.months.ago, Date.today.end_of_month)
   )
