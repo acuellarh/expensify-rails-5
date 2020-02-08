397609fd4af5d4fdd1069af718778df701e9cb44
Alejandro <acuellarh@gmail.com>
2020-02-08 09:19:19 -0500

    delete unnecesarry code

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -1,24 +1,3 @@
-$(document).ready(function(){
-  $('input[type=submit]').on('click', function (event) {
-    console.log("hello")
-    var form = $(this).parent('form');
+
+
     
-    event.preventDefault();
-
-    $.ajax({
-      type: "GET",
-      url: form.attr('action'),
-      data: form.serialize(),
-      success: function(data){
-        console,log(data)
-      },
-      error: function(data){
-        console.log("errorr")
-      },
-      
-      dataType: 'JSON'
-
-    });
-
-  });    
-});
